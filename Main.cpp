2af4f67a40b7b985f9cf42d0099b118e14135669
Jared <Bukinnear@gmail.com>
2018-10-13 21:37:44 +1100

    bug fixes, and minor changes

--- Main.cpp
+++ Main.cpp
@@ -30,6 +30,7 @@ void run_setup();
 void scan_library(std::filesystem::directory_entry dir);
 std::filesystem::directory_entry apps_directory(std::filesystem::directory_entry dir);
 void print_sub_directories(std::filesystem::directory_entry dir);
+bool is_valid_library(std::filesystem::directory_entry dir);
 
 std::vector<std::filesystem::directory_entry> libraryfilepaths;
 
@@ -45,8 +46,7 @@ int main()
 
     print_sub_directories(apps_directory(libraryfilepaths[0]));
 
-    std::cout << std::endl
-              << "finished" << std::endl;
+    std::cout << std::endl << "finished" << std::endl;
               
     // std::cout << "\n\nThe program will now exit.\n\nPress any key to continue..." << std::endl;
     // getch();
@@ -84,7 +84,7 @@ bool load_settings()
 {
     std::ifstream configfile("config.txt");
     if (!configfile.is_open())
-    {
+    {        
         return false;
     }
 
@@ -92,35 +92,70 @@ bool load_settings()
 
     while (configfile.getline(line, sizeof line))
     {
+        std::filesystem::directory_entry dir;
+
         if (std::filesystem::exists(line))
         {
-            libraryfilepaths.push_back(std::filesystem::directory_entry(line));
+            dir.assign(line);
+            if (is_valid_library(dir))
+            {
+                libraryfilepaths.push_back(dir);
+            }
+            else
+            {
+                std::cout << std::endl << "WARNING\n" << dir << "\nis not a valid steam directory (could not locate steamapps subfolder)." << std::endl << std::endl;                
+            }
         }
+        else
+        {
+            std::cout << std::endl << "WARNING\n\"" << line << "\"\nis not a valid filepath." << std::endl << std::endl;
+        }          
     }
-
+    
     configfile.close();
 
-    return true;
+    if (libraryfilepaths.empty())
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
 }
 
 void run_setup()
 {
     while (libraryfilepaths.empty())
     {
-        std::cout << std::endl << "There are no Steam libraries set up." << std::endl << "Please enter a valid filepath:" << std::endl;
+        std::cout << std::endl << "There are no Steam libraries set up." << std::endl << "Please enter a valid filepath (directory must contain steamapps subfolder):" << std::endl;
 
-        char inputbuffer[256];
+        char input_buffer[256];
 
-        std::cin.getline(inputbuffer, sizeof inputbuffer);
-        std::cout << inputbuffer << std::endl;
+        std::cin.getline(input_buffer, sizeof input_buffer);
 
-        if (!std::filesystem::exists(inputbuffer))
+        if (std::filesystem::exists(input_buffer))
+        {
+            bool valid_steam_library = false;
+            for (auto p : std::filesystem::directory_iterator(input_buffer))
+            {
+                if (p.path().filename().string() == "steamapps")
+                {
+                    valid_steam_library = true;                 
+                    libraryfilepaths.push_back(std::filesystem::directory_entry(input_buffer));
+                    break;
+                }
+            }
+            if (!valid_steam_library)
+            {
+                std::cout << "Could not locate steamapps in the provided filepath.";
+            }
+        }
+        else
         {
             std::cout << "path is invalid, or could not be accessed." << std::endl;
             continue;
         }
-
-        libraryfilepaths.push_back(std::filesystem::directory_entry(inputbuffer));
     }
 }
 
@@ -136,6 +171,18 @@ void scan_library(std::filesystem::directory_entry dir)
 std::filesystem::directory_entry apps_directory(std::filesystem::directory_entry dir)
 {
     return std::filesystem::directory_entry(dir.path().string().append("\\steamapps\\common"));
+}
+
+bool is_valid_library(std::filesystem::directory_entry dir)
+{
+    for (auto p : std::filesystem::directory_iterator(dir))
+    {
+        if (std::filesystem::directory_entry(p).path().filename().string() == "steamapps")
+        {
+            return true;
+        }
+    }
+    return false;
 }
 
 void print_sub_directories(std::filesystem::directory_entry dir)
