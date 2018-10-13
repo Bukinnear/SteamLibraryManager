e9a74fff165457b5c774e7d629426a68fdf7025e
Jared <Bukinnear@gmail.com>
2018-10-13 19:22:46 +1100

    added functionality to use multiple libraries

--- Main.cpp
+++ Main.cpp
@@ -20,41 +20,35 @@ struct app_details
     // name
     // number
     // filepath
-    // library id <- this will require error checking
+    // library id
 };
 
 void print_header();
-bool load_settings();
 bool save_settings();
+bool load_settings();
 void run_setup();
-void scan_directory(std::string s);
+void scan_library(std::filesystem::directory_entry dir);
+std::filesystem::directory_entry apps_directory(std::filesystem::directory_entry dir);
+void print_sub_directories(std::filesystem::directory_entry dir);
 
-std::filesystem::directory_entry libraryfilepath;
-std::filesystem::directory_entry libraryapps;
+std::vector<std::filesystem::directory_entry> libraryfilepaths;
 
 int main()
-{    
+{
     print_header();
 
     if (!load_settings())
     {
         run_setup();
         save_settings();
-    }  
-
-    std::cout << "-------------------------------------------\nListing all folders found in library " << libraryfilepath.path().string() << std::endl << "-------------------------------------------" << std::endl << std::endl;
-    {
-        int index = 1;
-        for (auto &p : std::filesystem::directory_iterator(libraryapps))
-        {
-            std::cout << p.path().filename().string() << std::endl;
-            index++;
-        }
     }
-    
-    std::cout << std::endl << "finished" << std::endl;
 
-    // std::cout << "Press any key to continue..." << std::endl;    
+    print_sub_directories(apps_directory(libraryfilepaths[0]));
+
+    std::cout << std::endl
+              << "finished" << std::endl;
+              
+    // std::cout << "\n\nThe program will now exit.\n\nPress any key to continue..." << std::endl;
     // getch();
 
     return 0;
@@ -62,11 +56,32 @@ int main()
 
 void print_header()
 {
-    std::cout << std::endl << "-------------------------------------------\n-                                         -\n-          Steam Library Manager          -\n-                                         -\n-------------------------------------------" << std::endl << std::endl;
+    std::cout << std::endl << "-------------------------------------------\n-                                         -\n-          Steam Library Manager          -\n-                                         -\n-------------------------------------------" << std::endl;
+}
+
+bool save_settings()
+{
+    std::ofstream savefile("config.txt", std::ios::out);
+
+    for (auto library : libraryfilepaths)
+    {
+        savefile << library.path().string();
+    }
+
+    if (savefile.fail() == 0)
+    {
+        savefile.close();
+        return true;
+    }
+    else
+    {
+        savefile.close();
+        return false;
+    }
 }
 
 bool load_settings()
-{    
+{
     std::ifstream configfile("config.txt");
     if (!configfile.is_open())
     {
@@ -79,57 +94,70 @@ bool load_settings()
     {
         if (std::filesystem::exists(line))
         {
-            libraryfilepath.assign(line);
-            libraryapps.assign( libraryfilepath.path().string().append("\\steamapps\\common"));
-            // std::cout << "library path is: " << libraryfilepath << std::endl;
+            libraryfilepaths.push_back(std::filesystem::directory_entry(line));
         }
     }
+
+    configfile.close();
 
     return true;
 }
 
-bool save_settings()
-{    
-    std::ofstream savefile("config.txt", std::ios::out);
-    savefile << libraryfilepath.path().string();
-    savefile.close();
-
-    if (savefile.fail() == 0)
-    {
-        return true;
-    }
-    else 
-    {
-        return false;
-    }
-}
-
 void run_setup()
 {
-    while (!libraryfilepath.exists())
+    while (libraryfilepaths.empty())
     {
-        std::cout << std::endl << "Library filepath is invalid." << std::endl << "Please enter a valid filepath:" << std::endl;        
+        std::cout << std::endl << "There are no Steam libraries set up." << std::endl << "Please enter a valid filepath:" << std::endl;
 
         char inputbuffer[256];
 
-        std::cin.getline(inputbuffer, sizeof inputbuffer);        
+        std::cin.getline(inputbuffer, sizeof inputbuffer);
         std::cout << inputbuffer << std::endl;
 
         if (!std::filesystem::exists(inputbuffer))
         {
-            std::cout << "path could not be found." << std::endl;
+            std::cout << "path is invalid, or could not be accessed." << std::endl;
             continue;
         }
-        
-        libraryfilepath.assign(inputbuffer);
-    }    
+
+        libraryfilepaths.push_back(std::filesystem::directory_entry(inputbuffer));
+    }
 }
 
-void scan_directory (std::filesystem::directory_entry dir)
+void scan_library(std::filesystem::directory_entry dir)
 {
     for (auto &p : std::filesystem::directory_iterator(dir))
     {
         std::cout << p << std::endl;
         // dl.push_back(p);
+    }
+}
+
+std::filesystem::directory_entry apps_directory(std::filesystem::directory_entry dir)
+{
+    return std::filesystem::directory_entry(dir.path().string().append("\\steamapps\\common"));
+}
+
+void print_sub_directories(std::filesystem::directory_entry dir)
+{
+    std::cout << std::endl << "-------------------------------------------\nListing all folders found in library " << dir.path().string() << std::endl << "-------------------------------------------" << std::endl << std::endl;
+    {
+        int index = 1;
+        std::string spacer = ".     ";
+
+        for (auto &p : std::filesystem::directory_iterator(dir))
+        {
+            if (index == 10)
+            {
+                spacer = ".    ";
+            }
+            else if (index == 100)
+            {
+                spacer = ".   ";
+            }
+
+            std::cout << index << spacer << p.path().filename().string() << std::endl;
+            index++;
+        }
     }
 }
