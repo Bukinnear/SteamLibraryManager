bcfbd4afe2990b1343fa4aa174926e8a4c617894
Jared <Bukinnear@gmail.com>
2019-12-23 12:42:32 +1100

    Changed ManifestReader to return a struct containing manifest data

--- Source/ManifestReader.cpp
+++ Source/ManifestReader.cpp
@@ -1,34 +1,65 @@
 #include "ManifestReader.h"
 
-
-const std::unique_ptr<std::unordered_map<std::string, std::string>> ManifestReader::Read(std::string path)
+const std::array<std::string, 7> AppManifest::ManifestFields = {"appid", "name", "StateFlags", "installdir", "SizeOnDisk", "BytesToDownload", "BytesDownloaded"};
+const bool AppManifest::ContainsField(std::string s)
 {
-    if (!std::filesystem::exists(path) || !std::filesystem::is_regular_file(path)) { return nullptr; }
+    for (auto a : ManifestFields)
+    {
+        if (a == s) { return true; }
+    }
+    return false;
+}
+
+const AppManifest ManifestReader::Read(std::string path)
+{
+    if (!std::filesystem::exists(path) || !std::filesystem::is_regular_file(path))
+    {
+        return AppManifest();
+    }
 
     std::ifstream file(path);
- 
-    std::string buffer;   
-    std::string key;
-    std::string value;
 
-    std::unique_ptr<std::unordered_map<std::string, std::string>> map(new std::unordered_map<std::string, std::string>);
+    std::string buffer;
+
+    std::unordered_map<std::string, std::string> map;
 
     while (std::getline(file, buffer))
     {
-        if (buffer[0] != '\t' || buffer[0] == '}') { continue; }
+        if (buffer[0] != '\t' || buffer[0] == '}')
+        {
+            continue;
+        }
 
+        std::string key;
+        std::string value;
         int offset = 2;
         int FirstEnd;
         if (offset >= buffer.length()) { break; }
 
         std::string key = buffer.substr(offset, (FirstEnd = buffer.find_first_of('\"', offset)) - offset);
+        if (!AppManifest::ContainsField(key)) { continue; }
 
         offset = FirstEnd + 4;
         if (offset >= buffer.length()) { break; }
 
         value = buffer.substr(offset, buffer.find_first_of('\"', offset) - offset);
 
-        map->insert({key, value});
+        map.insert({key, value});
+    }
+
+    try
+    {
+        int appid = std::stoi(map.at("appid"));
+        std::string name = map.at("name");
+        int stateflags = std::stoi(map.at("StateFlags"));
+        std::string installdir = map.at("installdir");
+        uintmax_t sizeondisk = std::stoi(map.at("SizeOnDisk"));
+        uintmax_t bytestodownload = std::stoi(map.at("BytesToDownload"));
+        uintmax_t bytesdownloaded = std::stoi(map.at("BytesDownloaded"));
+
+        return AppManifest(appid, name, stateflags, installdir, sizeondisk, bytestodownload, bytestodownload);
     }    
-    return map;
+    catch(const std::exception& e) { std::cerr << e.what() << '\n'; };     
+
+    return AppManifest();
 }
--- Source/ManifestReader.h
+++ Source/ManifestReader.h
@@ -4,11 +4,54 @@
 #include <fstream>
 #include <unordered_map>
 #include <filesystem>
-#include <memory>
+#include <array>
 
+struct AppManifest
+{
+public:
+    const int appid;
+    const std::string name;
+    const int StateFlags;
+    const std::string installdir;
+    const uintmax_t SizeOnDisk;
+    const uintmax_t BytesToDownload;
+    const uintmax_t BytesDownloaded;
+
+    AppManifest(
+    int Inp_appid,
+    std::string Inp_name,
+    int Inp_StateFlags,
+    std::string Inp_installdir,
+    uintmax_t Inp_SizeOnDisk,
+    uintmax_t Inp_BytesToDownload,
+    uintmax_t Inp_BytesDownloaded) : 
+        appid(Inp_appid),
+        name(Inp_name),
+        StateFlags(Inp_StateFlags),
+        installdir(Inp_installdir),
+        SizeOnDisk(Inp_SizeOnDisk),
+        BytesToDownload(Inp_BytesToDownload),
+        BytesDownloaded(Inp_BytesDownloaded)
+    { };
+
+    AppManifest() : 
+        appid(0),
+        name(""),
+        StateFlags(0),
+        installdir(""),
+        SizeOnDisk(0),
+        BytesToDownload(0),
+        BytesDownloaded(0)
+    { };
+
+    static const bool ContainsField(std::string);
+    
+private:
+    static const std::array<std::string, 7> ManifestFields;
+};
 
 class ManifestReader
 {
 public:
-    static const std::unique_ptr<std::unordered_map<std::string, std::string>> Read(std::string);
+    static const AppManifest Read(std::string);
 };
