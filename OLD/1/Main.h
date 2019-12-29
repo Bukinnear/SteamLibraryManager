c5749d2961145f7bd4f84d163ae155f129b5f182
Jared <Bukinnear@gmail.com>
2019-12-30 10:41:25 +1100

    Removed old project files

--- OLD/1/Main.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <filesystem>
-#include "library.h"
-
-bool save_settings();
-bool load_settings();
-void run_setup();
-
-std::vector<library> libraries;
--- OLD/2/Output.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-namespace output
-{
-    void print_header();
-    void print_sub_header(std::string sub_header);
-    std::string create_menu_item(int index, std::string menu_item);
-};
